468f3249a2079315c5abad02db1dd496295b6e65
ostap <ost.bregin2014@yandex.ua>
2017-11-29 21:28:30 +0200

    Added middleware for routing authorizations

--- routes/web.php
+++ routes/web.php
@@ -18,36 +18,40 @@ Route::get('/',[
 
 Route::group(['prefix' => 'user'],function (){
 
-    Route::get('/signup',[
-        'uses' => 'UserController@getSignup',
-        'as'   => 'user.signup'
-    ]);
 
-    Route::post('/signup',[
-        'uses' => 'UserController@postSignup',
-        'as'   => 'user.signup'
-    ]);
+    Route::group(['prefix' => 'quest'], function (){
+        Route::get('/signup',[
+            'uses' => 'UserController@getSignup',
+            'as'   => 'user.signup'
+        ]);
 
-    Route::get('/signin',[
-        'uses' => 'UserController@getSignin',
-        'as'   => 'user.signin'
-    ]);
+        Route::post('/signup',[
+            'uses' => 'UserController@postSignup',
+            'as'   => 'user.signup'
+        ]);
 
-    Route::post('/signin',[
-        'uses' => 'UserController@postSignin',
-        'as'   => 'user.signin'
-    ]);
+        Route::get('/signin',[
+            'uses' => 'UserController@getSignin',
+            'as'   => 'user.signin'
+        ]);
 
-});
+        Route::post('/signin',[
+            'uses' => 'UserController@postSignin',
+            'as'   => 'user.signin'
+        ]);
 
-Route::group(['prefix' => 'auth'], function(){
-    Route::get('/profile',[
-        'uses'  =>  'UserController@getProfile',
-        'as'    =>  'user.profile'
-    ]);
+    });
 
-    Route::get('/logout',[
-        'uses'  =>  'UserController@getLogout',
-        'as'    =>  'user.logout'
-    ]);
+    Route::group(['prefix' => 'auth'], function (){
+
+        Route::get('/profile',[
+            'uses'  =>  'UserController@getProfile',
+            'as'    =>  'user.profile'
+        ]);
+
+        Route::get('/logout',[
+            'uses'  =>  'UserController@getLogout',
+            'as'    =>  'user.logout'
+        ]);
+    });
 });
